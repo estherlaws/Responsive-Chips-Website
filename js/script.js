97363703ac0669be4ad02c0e6c0a2eaba3d50577
estherlaws <estherelaws@gmail.com>
2024-05-19 23:12:18 -0400

    Care Section

--- js/script.js
+++ js/script.js
@@ -1,32 +1,42 @@
 // Show Menu
 const navMenu = document.getElementById("nav-menu"),
     navToggle = document.getElementById("nav-toggle"),
-    navClose = document.getElementById("nav-close")
+    navClose = document.getElementById("nav-close");
 
-if(navToggle) {
-    navToggle.addEventListener("click", () =>{
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
         navMenu.classList.add("show-menu");
-    })
+    });
 }
 
-if(navClose) {
-    navClose.addEventListener("click", () =>{
-        navMenu.classList.add("show-menu");
-    })
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
 }
 
 // Close Menu
-const navLink = document.querySelectorAll(".nav-link")
+const navLink = document.querySelectorAll(".nav-link");
 
 const linkAction = () => {
-    const navMenu = document.getElementById("nav-menu")
-    navMenu.classList.remove("show-menu") 
-}
-navLink.forEach(n => n.addEventListener("click", linkAction))
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+};
+navLink.forEach(n => n.addEventListener("click", linkAction));
 
 // Change Header Background
 const shadowHeader = () => {
-    const header = document.getElementById("header")
-    this.scrollY >= 50 ? header.classList.add("shadow-header")
-                       : header.classList.remove("shadow-header")
-}
+    const header = document.getElementById("header");
+    window.scrollY >= 50 ? header.classList.add("shadow-header")
+                         : header.classList.remove("shadow-header");
+};
+
+window.addEventListener('scroll', shadowHeader);
+
+// Swiper
+const swiperFavorites = new Swiper(".favorites-swiper", {
+    loop: true,
+    grabCursor: true,
+    slidesPerView: "auto",
+    centeredSlides: "auto",
+});
