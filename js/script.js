4cc739fa9e658e5c5dd4ef32fcf700fa17971627
estherlaws <estherelaws@gmail.com>
2024-05-21 04:33:31 -0400

    Added: Media Queries

--- js/script.js
+++ js/script.js
@@ -1,42 +1,76 @@
 // Show Menu
 const navMenu = document.getElementById("nav-menu"),
-    navToggle = document.getElementById("nav-toggle"),
-    navClose = document.getElementById("nav-close");
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 if (navToggle) {
-    navToggle.addEventListener("click", () => {
-        navMenu.classList.add("show-menu");
-    });
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 if (navClose) {
-    navClose.addEventListener("click", () => {
-        navMenu.classList.remove("show-menu");
-    });
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 // Close Menu
 const navLink = document.querySelectorAll(".nav-link");
 
 const linkAction = () => {
-    const navMenu = document.getElementById("nav-menu");
-    navMenu.classList.remove("show-menu");
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 };
-navLink.forEach(n => n.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // Change Header Background
 const shadowHeader = () => {
-    const header = document.getElementById("header");
-    window.scrollY >= 50 ? header.classList.add("shadow-header")
-                         : header.classList.remove("shadow-header");
+  const header = document.getElementById("header");
+  window.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
 };
 
-window.addEventListener('scroll', shadowHeader);
+window.addEventListener("scroll", shadowHeader);
 
 // Swiper
 const swiperFavorites = new Swiper(".favorites-swiper", {
-    loop: true,
-    grabCursor: true,
-    slidesPerView: "auto",
-    centeredSlides: "auto",
+  loop: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
 });
+
+// Scroll Up
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
+
+// Scroll Sections: Active 
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav-menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive)
